09392e3f41b32adfb608b10e7b3aa901159df036
manjor <manoeltavares.jr@gmail.com>
2018-10-26 15:15:27 -0300

    Components with parameters

---
+++ fundamentos/react/exercicios/src/components/goodmorning.jsx
@@ -0,0 +1,21 @@
+/**
+ * Created By: Manoel Tavares
+ * Date: 26 - 10 - 2018
+ */
+import React, {Fragment} from 'react'
+
+//export default () => <h2>Good Morning</h2>
+//Props define that function accept parameters
+export default props => 
+    //for two or more tags, is necessary envolve with tag super
+    //or use React.Fragment
+    <Fragment>
+        <h2>Good Morning {props.name}!</h2>
+        <h3>Até Breve</h3>
+   </Fragment>
+
+//or use array of elements with keys
+//export default props => [
+//    <h2 key='h1'>Good Morning {props.name}!</h2>,
+//    <h3 key='ok'>You Okay?</h3>
+//]
--- fundamentos/react/exercicios/src/index.js
+++ fundamentos/react/exercicios/src/index.js
@@ -5,6 +5,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import First from './components/first'
+import GoodMorning from './components/goodmorning'
 
 //ReactDom to make DOM changes
 //Render a h1 in DOM whose  id is = root
@@ -12,5 +13,6 @@ import First from './components/first'
 //const elemento = <h1>Hello React</h1>
 //ReactDom.render(elemento, document.getElementById('root'))
 
-ReactDom.render(<First />, document.getElementById('root'))
+//ReactDom.render(<First />, document.getElementById('root'))
+ReactDom.render(<GoodMorning name='Manoel'/>, document.getElementById('root'))
 
