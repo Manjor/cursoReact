6fa280d820c1e18a9f573143147806509350e493
Manoel <manoeltavares.jr@gmail.com>
2018-10-29 14:25:18 -0300

    Component Calculator

---
+++ project_calculator/src/main/Calculator.jsx
@@ -0,0 +1,22 @@
+//Components of Calculator in JSX
+//imports
+import React,{Component} from 'react'
+import './Calculator.css'
+
+//Criate class Calcultator in default export
+export default class Calculator extends Component{
+
+    //Function render
+    render(){
+
+        return (
+            //In JSX use className nothing class
+            <div className='calculator'>
+                
+            </div>
+        )
+    }
+
+
+
+}
