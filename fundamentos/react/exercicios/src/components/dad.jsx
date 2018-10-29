27cc0b1eace75b57cc2331baa88403920408d3d1
Manoel <manoeltavares.jr@gmail.com>
2018-10-29 13:55:44 -0300

    Father&SOn Components II

--- fundamentos/react/exercicios/src/components/dad.jsx
+++ fundamentos/react/exercicios/src/components/dad.jsx
@@ -4,13 +4,19 @@
  */
 import React from 'react'
 import Sun from './son'
+import {childrenWithProps} from '../utils/utils'
 
 export default props =>
     <div>
         <h1>{props.name} {props.lastname}</h1>
+        <h2>Filhos</h2>
         <ul>
-            <Sun name='Manoel' lastname={props.lastname}/>
+            {/*<Sun name='Manoel' lastname={props.lastname}/>
             <Sun {...props}/>
-            <Sun {...props} name='Adria'/>
+            <Sun {...props} name='Adria'/> */}
+            {/*props.children*/}
+            {
+                childrenWithProps(props)
+            }
         </ul>
     </div>
--- fundamentos/react/exercicios/src/index.js
+++ fundamentos/react/exercicios/src/index.js
@@ -6,10 +6,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Salutations from './components/salutation'
 import Dad from './components/dad'
+import Son from './components/son'
 
 ReactDom.render(
     <div>
         <Dad name='Manoel' lastname='Tavares'>
+            <Son name='Junior'/>
+            <Son name='Fabiano'/>
+            <Son name='Oliveira'/>
             {/* how to pass the child components */}
         </Dad>
         
